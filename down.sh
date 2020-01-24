a17a656ac1860bbd179493cacefe12c601cffafb
Andy Wright <atwright147@gmail.com>
2020-01-24 10:35:07 +0000

    add experimental shutdown script

---
+++ down.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+echo "Shutdown docker services";
+docker-compose down;
+
+echo "Clear all downloaded images"
+docker rmi $(docker images -a -q) -f;
+docker volume rm $(docker volume ls -q) -f;
+
+echo "Remove OpenVPN server lists";
+rm -rf /volume2/docker/media-server/.config/vpn/ovpn
